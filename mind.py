ca3a43a81c549363d9d218430f974089836b48d9
masghari96 <jacky220000@gmail.com>
2022-02-07 10:02:27 +0330

    Operators added

--- mind.py
+++ mind.py
@@ -1,3 +1,10 @@
 firstNumber = input("Please choose a number between 1 to 9 :")
 firstNumber = int(firstNumber)
 Number = firstNumber
+Number *= 2
+Number += 8
+Number += firstNumber
+Number -= 2
+Number //= 3
+Number -= firstNumber
+Number *= 4
